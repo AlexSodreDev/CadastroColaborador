115f3f36f66c8a12296d20c523d8a0336ff54fef
AlexSodreDev <alexandre.cortines@gmail.com>
2021-04-06 21:25:35 -0300

    route + controller para deletar usuario

--- src/controllers/EmployeesController.ts
+++ src/controllers/EmployeesController.ts
@@ -55,6 +55,34 @@ class EmployeesController {
 
         return response.status(200).json(employeeFindId)
     }
+
+    /**async toUpdate(request: Request, response: Response) {
+        const employeesRepository = getCustomRepository(EmployeesRepository);
+
+        const employeeId = parseInt(request.params.id)
+
+        const employeeFindId = await employeesRepository.findOne({ id: employeeId })
+
+        const toUpdate = 
+
+        if (!employeeFindId) {
+            return response.sendStatus(404).json({
+                error: "Id not found!"
+            });
+        }
+
+        return response.status(200).json(employeeFindId)
+   } */
+
+    async delete(request: Request, response: Response) {
+        const employeesRepository = getCustomRepository(EmployeesRepository);
+
+        const employeeId = request.params.id
+
+        const deleteEmployee = await employeesRepository.delete(employeeId)
+
+        return response.status(204).json(deleteEmployee)
+    }
 }
 
 export { EmployeesController };
--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,8 @@ const router = Router();
 const employeesController = new EmployeesController();
 
 router.post("/employees", employeesController.create);
-router.get('/employees', employeesController.index);
+router.get("/employees", employeesController.index);
 router.get("/employees/:id", employeesController.findById);
-
+//router.patch("/employees/:id", employeesController.toUpdate);
+router.delete("/employees/:id", employeesController.delete);
 export { router };
